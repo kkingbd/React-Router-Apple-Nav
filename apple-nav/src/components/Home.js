bcb129abcdf8d070957e8708ff0e6597a9e42e2f
Md Kawsar Hussen <mdkawsarhussen@Mds-MacBook-Pro.local>
2019-02-26 20:57:17 -0500

    Home Nav is styled now

--- apple-nav/src/components/Home.js
+++ apple-nav/src/components/Home.js
@@ -1,22 +1,44 @@
 import React from 'react'
 import { NavLink } from "react-router-dom";
+import styled from 'styled-components';
+
+const Nav = styled.div`
+ display : flex;
+ flex-wrap: wrap;
+ justify-content: space-around;
+ background: black;
+ max-height: 100px;
+ align-items: center;
+ padding: 1%;
+ .appleLogo{
+     height: 70px;
+     width: 70px;
+ }
+ .main-nav{
+   color: white;
+   cursor: pointer;
+ }
+ .main-nav:hover{
+       color: red;
+   }
+`;
 
 const Home = props =>{
     return(
-        <nav>
+        <Nav>
             <NavLink to='/'>
-                <img src = 'http://logok.org/wp-content/uploads/2014/04/Apple-logo-grey-880x625.png' alt= 'apple Icon' />
+                <img className= 'appleLogo' src = 'http://logok.org/wp-content/uploads/2014/04/Apple-logo-grey-880x625.png' alt= 'apple Icon' />
             </NavLink>
             {props.data.map( item =>(
                 <div 
                     key ={item.name}
-                    className = 'main-nav-item'
+                    className = 'main-nav'
                     onClick= { () => props.history.push(`/${item.name}`)}
                 >
                   {item.name}
                 </div>
             ))}
-        </nav>
+        </Nav>
     );
 };
 export default Home;
